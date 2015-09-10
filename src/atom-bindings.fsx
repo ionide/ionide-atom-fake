84f56ebe57d139897338d3958f493261157770de
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-09-11 00:26:06 +0200

    Base commit

---
+++ src/atom-bindings.fsx
@@ -0,0 +1,16 @@
+// --------------------------------------------------------------------------------------
+// Reference all the TypeScript mappings Atom package might need
+// --------------------------------------------------------------------------------------
+
+#if INTERACTIVE
+#I "../lib"
+#I "../packages/FunScript/lib/net40"
+
+#r "FunScript.dll"
+#r "FunScript.Interop.dll"
+#r "FunScript.TypeScript.Binding.atom.dll"
+#r "FunScript.TypeScript.Binding.jquery.dll"
+#r "FunScript.TypeScript.Binding.lib.dll"
+#r "FunScript.TypeScript.Binding.node.dll"
+
+#endif
